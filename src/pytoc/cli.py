25231b002831f034ac30a255d93a9d8735835110
mky2 <51354328+mky2@users.noreply.github.com>
2024-11-18 09:53:06 +0000

    new command fixw

--- src/pytoc/cli.py
+++ src/pytoc/cli.py
@@ -1,13 +1,24 @@
 from typing import Optional
 import typer
-from .main import fix_toc
+from .main import fix_toc, fix_width
 
 app = typer.Typer()
 
 @app.command()
-def main(input: str, json: str, output: Optional[str] = None):
+def add(input: str, json: str, output: Optional[str] = None):
     if output == None:
-        output = f'(fixed) {input}'
+        output = _output_name(input)
     with open(json, 'r') as f:
         fix_toc(input, output, f.read())
         print(f'Done fixing `{input}`')
+
+@app.command()
+def fixw(input: str, output: Optional[str] = None):
+    if output == None:
+        output = _output_name(input)
+    fix_width(input, output)
+    print(f'Done')
+
+
+def _output_name(input: str):
+    return f'(fixed) {input}'
--- src/pytoc/main.py
+++ src/pytoc/main.py
@@ -1,6 +1,6 @@
 from typing import List
+from pikepdf import Pdf, Page
 from .outline import OutlineData
-from pikepdf import Pdf
 
 
 def fix_toc(input: str, output: str, js: str):
@@ -9,4 +9,27 @@ def fix_toc(input: str, output: str, js: str):
         with pdf.open_outline() as outline:
             outline.root.clear()
             outline.root.extend([child.create(pdf) for child in outline_data])
+        pdf.save(output)
+
+
+def _update_item(item: OutlineData):
+    # outline_info = OutlineInfo(item.title, item.destination[0])
+    if item.action and "/D" in item.action:
+        dest = Page(item.action["/D"][0]).label
+    elif item.destination:
+        dest = Page(item.destination[0]).label
+    if item.children:
+        children = [_update_item(child) for child in item.children]
+    else:
+        children = None
+    # print(dest)
+    return OutlineData(item.title, dest, children)
+
+
+def fix_width(input, output):
+    with Pdf.open(input) as pdf:
+        with pdf.open_outline() as outline:
+            updated_items = [_update_item(i).create(pdf) for i in outline.root]
+            outline.root.clear()
+            outline.root.extend(updated_items)
         pdf.save(output)
